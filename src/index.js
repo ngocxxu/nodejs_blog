4c90c502ab52ad08e8dfaa4f32b23ce6a226d67b
ngocxxu <ngocquach43@gmail.com>
2022-06-18 20:42:59 +0700

    learning about post method

--- src/index.js
+++ src/index.js
@@ -8,7 +8,16 @@ const app = express();
 const port = 3000;
 
 // Check folder default for file static will be into public folder
-app.use(express.static(path.join(__dirname, 'public'))) // === localhost:3000/public
+app.use(express.static(path.join(__dirname, "public"))); // === localhost:3000/public
+
+// Apply Middleware
+// Can read body from Post method
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+); 
+app.use(express.json());
 
 // HTTP logger
 app.use(morgan("combined"));
@@ -29,6 +38,15 @@ app.get("/", (req, res) => {
 
 app.get("/news", (req, res) => {
   res.render("news");
+});
+
+app.get("/search", (req, res) => {
+  res.render("search");
+});
+
+app.post("/search", (req, res) => {
+  console.log(req.body);
+  res.send("");
 });
 
 app.listen(port, () => {
